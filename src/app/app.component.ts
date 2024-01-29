f70ee58e6c13c7eb17e1c9ecd24ac9ff6ff42cf3
Vivek Kumar <149459101+dvivek1kumar@users.noreply.github.com>
2024-01-29 11:28:20 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angularSprint2';
+  myObservable = new Observable((observer) => {
+
+    console.log('Observable Starts');
+    observer.next('1');
+    observer.next('2');
+    observer.next('3');
+    observer.next('4');
+    observer.next('5');
+  });
+  ngOnInit(): void {
+
+    this.myObservable.subscribe((val) =>{
+   console.log(val);})
+    }
+}
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookAdditionComponent } from './book-addition/book-addition.component';
+import { BookReportComponent } from './book-report/book-report.component';
+import { BookUpdateComponent } from './book-update/book-update.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookListComponent,
+    BookAdditionComponent,
+    BookReportComponent,
+    BookUpdateComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
